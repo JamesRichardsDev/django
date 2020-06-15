0af5814ab3c703a87a785b9d7c22f30441bca22f
James Richards <jamesrichards@Jamess-MacBook-Pro.local>
2020-06-14 21:54:01 -0400

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,5 +4,8 @@ from django.urls import re_path
 from lists.views import home_page
 
 urlpatterns = [
-    re_path(r'^$', home_page, name='home')
+    re_path(r'^$', home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+    ),
 ]
