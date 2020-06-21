3d061738af310ce43cb179abc526ced5b5e3c8ec
James Richards <jamesrichards@Jamess-MacBook-Pro.local>
2020-06-21 18:49:22 -0400

    fixed urlpatterns

--- lists/urls.py
+++ lists/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import re_path
-from lists.views import home_page
+from lists.views import home_page, view_list, add_item, new_list
 
 urlpatterns = [
-    re_path(r'^$', 'lists.views.home_page', name='home'),
-    re_path(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    re_path(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^$', home_page, name='home'),
+    url(r'^lists/(\d+)/$', view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', add_item, name='add_item'),
+    url(r'^lists/new$', new_list, name='new_list'),
 ]
