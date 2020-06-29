b3203296b67db0b1eb2f70161ad657498202e3b5
James Richards <jamesrichards@Jamess-MacBook-Pro.local>
2020-06-28 22:50:42 -0400

    implement duplicate item validation at model layer

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import ItemForm
+from lists.models import Item, List
 
 
 class ItemFormTest(TestCase):
@@ -13,6 +14,15 @@ class ItemFormTest(TestCase):
         self.assertIn('class="form-control input-lg"', form.as_p())
         
     def test_form_validation_for_blank_items(self):
+        EMPTY_LIST_ERROR = "You can't have an empty list item"
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
